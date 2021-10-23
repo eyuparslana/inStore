d2a194b1459252d456276e976f1db6b9d3a2f11c
eyuparslana <eyuparslana@gmail.com>
2021-10-23 20:52:06 +0300

    change config

--- config/config.go
+++ config/config.go
@@ -7,6 +7,6 @@ import (
 //EXPORT_FILE_PATH: Defines the path of the JSON file.
 //API_PORT: Defines which port the API will run on.
 var (
-	EXPORT_FILE_PATH = utils.GetEnv("EXPORT_FILE_PATH", "/tmp/inStoreData.json")
+	EXPORT_FILE_PATH = utils.GetEnv("EXPORT_FILE_PATH", "/tmp/")
 	API_PORT         = utils.GetEnv("API_PORT", "8000")
 )
